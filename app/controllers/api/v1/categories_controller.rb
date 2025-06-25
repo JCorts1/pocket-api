87cb55b697b5384eba99ef6913d4abe956b03fd6
Jay Cortes <cortesjay1991@gmail.com>
2025-06-25 17:56:48 +0200

    fixing categories

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,21 +4,26 @@ class Api::V1::CategoriesController < ApplicationController
 
   # GET /api/v1/categories
   def index
-    # Show default categories AND the user's own categories
-    @default_categories = Category.where(is_default: true)
-    @user_categories = current_user.categories
-    @categories = (@default_categories + @user_categories).uniq.sort_by(&:name)
+    # This single query fetches all categories where `is_default` is true,
+    # OR where the `user_id` matches the currently logged-in user.
+    # The .order(:name) sorts them alphabetically in the database.
+    @categories = Category.where(is_default: true)
+                          .or(Category.where(user_id: current_user.id))
+                          .order(:name)
+
     render json: @categories
   end
 
   # GET /api/v1/categories/:id
   def show
-    # set_category ensures we only find a category the user is allowed to see
+    # This action is correct and doesn't need changes.
+    # set_category ensures we only find a category the user is allowed to see.
     render json: @category.to_json(include: :expenses)
   end
 
   # POST /api/v1/categories
   def create
+    # This action is correct and doesn't need changes.
     @category = current_user.categories.build(category_params) # Associate with user
     # is_default will be false by default
     if @category.save
@@ -31,6 +36,7 @@ class Api::V1::CategoriesController < ApplicationController
   private
 
   def set_category
+    # This logic is correct and doesn't need changes.
     # A user can see a default category or one they created themselves.
     # This prevents a user from seeing another user's custom categories.
     @category = Category.where(is_default: true).or(Category.where(user: current_user)).find(params[:id])
